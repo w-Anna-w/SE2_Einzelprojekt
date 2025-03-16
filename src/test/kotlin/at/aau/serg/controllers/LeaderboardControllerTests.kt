2bdd97e22a97c0d6000fc15eadf2210c16396b02
Anna Walder <annapalomawa@edu.aau.at>
2025-03-16 17:22:43 +0100

    LeaderboardControllerTests test angepasst

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -37,7 +37,7 @@ class LeaderboardControllerTests {
         assertEquals(third, res[2])
     }
 
-    @Test
+   /* @Test
     fun test_getLeaderboard_sameScore_CorrectIdSorting() {
         val first = GameResult(1, "first", 20, 20.0)
         val second = GameResult(2, "second", 20, 10.0)
@@ -52,6 +52,24 @@ class LeaderboardControllerTests {
         assertEquals(first, res[0])
         assertEquals(second, res[1])
         assertEquals(third, res[2])
+    }
+    */
+
+    @Test
+    fun test_getLeaderboard_sameScore_CorrectTimeSorting() {
+        val first = GameResult(1, "first", 20, 20.0)
+        val second = GameResult(2, "second", 20, 10.0)
+        val third = GameResult(3, "third", 20, 15.0)
+
+        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
+
+        val res: List<GameResult> = controller.getLeaderboard()
+
+        verify(mockedService).getGameResults()
+        assertEquals(3, res.size)
+        assertEquals(first, res[2])
+        assertEquals(second, res[0])
+        assertEquals(third, res[1])
     }
 
 }
